aadb67038c7260292f4307c53d649ca83dbf0634
GwerhBard <lalo@perronegro.cl>
2022-12-25 02:26:42 -0300

    Estado Inicial

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ server.use((req, res, next)=>{
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept');
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
         next();
+        // }
     }
-// }
 );
 
 server.use(morgan('dev'))
