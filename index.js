119feb134df15256c72e74852fd42e8b65e504c0
GwerhBard <lalo@perronegro.cl>
2022-12-25 03:24:19 -0300

    Estado Inicial

--- index.js
+++ index.js
@@ -10,21 +10,29 @@ const posts = require('./Routes/apps/posts.js');
 
 server.use((req, res, next)=>{
     console.log(req.headers.origin)
-    // const corsList = [
-    //     'http://localhost:3000',
-    //     'http://localhost:3001',
-    //     'http://localhost:3002',
-    //     'http://192.168.100.16:3000'
-    // ];
-    // if(corsList.includes(req.headers.origin)){   
+    const corsList = [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://localhost:3002',
+        'https://laruinarecords.cl',
+        'https://tv.laruinarecords.cl'
+    ];
+    if(corsList.includes(req.headers.origin)){   
+        res.header('Access-Control-Allow-Origin', (req.headers.origin));
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        next();
+        }
+    if(req.headers['user-agent'].includes('insomnia')) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Credentials', 'true');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept');
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
         next();
-        // }
+
     }
-);
+});
 
 server.use(morgan('dev'))
 server.use(express.json())
