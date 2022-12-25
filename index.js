fe115df5f9defd4d50de0fc4fdfff1951b51d2cf
GwerhBard <lalo@perronegro.cl>
2022-12-25 02:25:16 -0300

    Estado Inicial

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ server.use((req, res, next)=>{
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
         next();
     }
-// });
+// }
+);
 
 server.use(morgan('dev'))
 server.use(express.json())
