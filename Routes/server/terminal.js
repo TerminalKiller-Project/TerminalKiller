7c815368d683e9b587788c66e3f537e151083c9a
GwerhBard <lalo@perronegro.cl>
2022-12-25 02:43:21 -0300

    Estado Inicial

--- Routes/server/terminal.js
+++ Routes/server/terminal.js
@@ -2,7 +2,8 @@ const express = require('express')
 const server = express.Router()
 
 server.get('/status',(req,res)=>{
-    res.status(200).res("OK")
+    console.log(req.body)
+    res.status(200).json({server:{status: 200, state:"Online"}})
 })
 
 module.exports = server
